80ccd56da5347122b0a89fbef4eef61363df1075
khtrist <119412552+khtrist@users.noreply.github.com>
2022-12-11 15:29:26 +0400

    Add files via upload

---
+++ 11 dekabr ev tapsirigi 4 cu.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yazini daxil edin");
+            var soz = Console.ReadLine();
+
+            char char1 = '1';
+            char char2 = '2';
+            char char3 = '3';
+            char char4 = '4';
+            char char5 = '5';
+            char char6 = '6';
+            char char7 = '7';
+            char char8 = '8';
+            char char9 = '9';
+            char char0 = '0';
+
+            for (int i = 0; i < soz.Length; i++)
+            {
+                for (int j = 0; j < soz.Length; j++)
+                {
+
+                    if (char0 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var  ");
+                        Console.WriteLine("Tekrar cehd edin");
+                            
+                         Console.ReadLine();
+                        continue;
+
+                       
+                    }
+                   
+
+                    if (char1 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+                    }
+                    
+
+                    if (char2 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+                    }
+
+                    if (char3 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+
+                    }
+
+
+                    if (char4 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+                    }
+
+
+                    if (char5 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem  var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+                    }
+
+
+                    if (char6 == soz[i])
+                    {
+                        Console.WriteLine("Sozde  Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+                    }
+
+
+                    if (char7 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+                    }
+
+
+                    if (char8 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+                    }
+
+
+                    if (char9 == soz[i])
+                    {
+                        Console.WriteLine("Sozde Reqem var ")
+                            ;
+                        Console.WriteLine("Tekrar cehd edin");
+
+                        Console.ReadLine();
+                        continue;
+
+                    }
+                    
+
+
+                }
+
+
+
+
+            }
+
+        }
+    }
+}
